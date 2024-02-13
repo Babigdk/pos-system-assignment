0d7d86f79bdb9b13856706f4bd26bcec7d209d8f
Babigdk <Babigdk@gmail.com>
2024-02-13 14:58:43 -0800

    Added ShoppingCart component structure

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {/* Display shopping cart items here */}
+    </div>
+  );
+};
+
+export default ShoppingCart;
