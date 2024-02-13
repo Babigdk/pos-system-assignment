0f7bd901208fd74a7c5779d7c4fe97954b367b2e
Babigdk <Babigdk@gmail.com>
2024-02-13 14:36:25 -0800

    Added React Router configuration in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+import Checkout from './components/Checkout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+          <Route path="/checkout" component={Checkout} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
